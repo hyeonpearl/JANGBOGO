3273d896ac646cca161cf5f2e0a3aa2e4f7c8efe
최현철 <hc9064@gmail.com>
2022-05-19 14:45:05 +0900

    프로젝트 통합으로 인한 코드 수정

--- Back/Web/resister/resister_main.js
+++ Back/Web/resister/resister_main.js
@@ -1,5 +1,5 @@
 function backHome() {
     var btn = document.getElementById('resister__form_cancle');
 
-    location.replace("/Web/home/home_index.html");
+    location.replace("/Back/Web/home/home_index.html");
 }
