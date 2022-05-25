92e510a9f289b3c876b0ef3e61fc7a00f44983df
SBSun <46879319+SBSun@users.noreply.github.com>
2022-05-25 11:52:06 +0900

    회원 시스템 강의 Test

---
+++ Back/jangbogo-spring/jangbogo-spring/src/main/java/jangbogo/jangbogospring/controller/MemberController.java
@@ -0,0 +1,11 @@
+package jangbogo.jangbogospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+import jangbogo.jangbogospring.service.MemberService;
+
+@Controller
+public class MemberController {
+    
+    private final MemberService memberService;
+}
