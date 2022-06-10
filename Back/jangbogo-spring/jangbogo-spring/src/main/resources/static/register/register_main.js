1ecf5700b6abba9df52ed03610710355c1896a06
SBSun <46879319+SBSun@users.noreply.github.com>
2022-06-10 13:27:02 +0900

    Email 중복확인 예외처리

--- Back/jangbogo-spring/jangbogo-spring/src/main/resources/static/register/register_main.js
+++ Back/jangbogo-spring/jangbogo-spring/src/main/resources/static/register/register_main.js
@@ -6,20 +6,34 @@ function backHome() {
 
 function checkEmail(){
     var email = document.getElementById('register__email').value;
+    var email_regex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 
-    $.ajax({
-        async: true,
-        type:'POST',
-        data:{email : email},
-        url:"/emailCheck",
-        dataType:"json",
-        success: function(data){
-            if(data == 1){
-                alert("존재하는 이메일입니다.");
+    if(email == ""){
+        alert("이메일을 입력해주세요.");
+        return;
+    }
+
+    if(email_regex.test(email))
+    {
+        $.ajax({
+            async: true,
+            type:'POST',
+            data:{email : email},
+            url:"/emailCheck",
+            dataType:"json",
+            success: function(data){
+
+                if(data == 1){
+                    alert("존재하는 이메일입니다.");
+                }
+                else{
+                    alert("사용가능한 이메일입니다.");
+                }
             }
-            else{
-                alert(email);
-            }
-        }
-    });
+        });
+    }
+    else
+    {
+        alert("이메일 형식으로 작성해주세요.");
+    }
 }
