af594ef7f9a52e2a34dc44efa05d05c73dfe2e83
SBSun <46879319+SBSun@users.noreply.github.com>
2022-07-10 00:58:51 +0900

    WebSecurityConfig - css, js, image 등 리소스 사용 권한 추가, 리소스 파일 위치 조정

--- Back/jangbogo-spring/jangbogo-spring/src/main/java/jangbogo/jangbogospring/config/WebSecurityConfig.java
+++ Back/jangbogo-spring/jangbogo-spring/src/main/java/jangbogo/jangbogospring/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package jangbogo.jangbogospring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,13 +16,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity  // Spring Security를 활성화 시킨다.
 public class WebSecurityConfig {
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 // HttpServletRequest에 따라 접근(access)을 제한
                 // antMatchers() 메서드로 특정 경로를 지정하며, permitAll(), hasRole() 메서드로 역할(Role)에 따른 접근 설정을 잡아준다.
                 .authorizeRequests()
-                    .antMatchers(   "/", "/home").permitAll() // 해당 경로들에 대해서는 권한없이 접근 가능
+                    .antMatchers(   "/", "/login", "/register", "/css/**", "/js/**", "/File/**").permitAll() // 해당 경로들에 대해서는 권한없이 접근 가능
                     .anyRequest().authenticated()   // 모든 요청에 대해, 인증된 사용자만 접근하도록 설정
                 .and()
                 // form 기반으로 인증을 하도록 합니다. 로그인 정보는 기본적으로 HttpSession을 이용
