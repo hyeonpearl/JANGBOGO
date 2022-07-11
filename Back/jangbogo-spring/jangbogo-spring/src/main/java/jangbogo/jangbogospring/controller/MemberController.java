257b27c04062529440b0e136d4a6f4a8cb29d39a
SBSun <46879319+SBSun@users.noreply.github.com>
2022-07-11 22:25:17 +0900

    로그인 시 favorite 페이지 이동 가능

--- Back/jangbogo-spring/jangbogo-spring/src/main/java/jangbogo/jangbogospring/controller/MemberController.java
+++ Back/jangbogo-spring/jangbogo-spring/src/main/java/jangbogo/jangbogospring/controller/MemberController.java
@@ -50,4 +50,9 @@ public class MemberController {
     public String editInfo(){
         return "member/edit-info";
     }
+
+    @GetMapping("/favorite")
+    public String favorite() {
+        return "member/favorite";
+    }
 }
